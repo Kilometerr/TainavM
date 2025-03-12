3f6b55c15800686661360b1c95d65097e75c4678
tnowicki <tymoteusz128@gmail.com>
2025-03-12 19:48:38 +0100

    Add logging

--- src/main/java/com/server/tainav/statistics/repository/StatisticsRepositoryImpl.java
+++ src/main/java/com/server/tainav/statistics/repository/StatisticsRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.server.tainav.statistics.repository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -7,15 +9,20 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class StatisticsRepositoryImpl implements StatisticsRepository {
 
+    private static final Logger logger = LoggerFactory.getLogger(StatisticsRepositoryImpl.class);
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @Override
     public boolean checkConnection() {
         try {
+            logger.debug("Attempting to check database connection");
             jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+            logger.info("Database connection successful");
             return true;
         } catch (Exception e) {
+            logger.error("Database connection failed", e);
             return false;
         }
     }
--- src/main/java/com/server/tainav/statistics/service/HealthCheckService.java
+++ src/main/java/com/server/tainav/statistics/service/HealthCheckService.java
@@ -1,7 +1,6 @@
 package com.server.tainav.statistics.service;
 
 import com.server.tainav.statistics.exception.HealthCheckException;
-import org.springframework.stereotype.Service;
 
 public interface HealthCheckService {
 
